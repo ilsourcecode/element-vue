46bb21dda6c9b7c7436a0b3fee623d6d2914afe1
tom_os <tom_os@qq.com>
2021-07-08 17:59:12 +0800

    add some codes

--- src/views/Input.vue
+++ src/views/Input.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="input">
     <h1>我是Input组件</h1>
+    用户名：<el-input size="small" v-model="input" :disabled="false" placeholder="请输入内容"></el-input>
+    密码：<el-input v-model="inputPw" placeholder="请输入密码" show-password></el-input>
   </div>
 </template>
 
 <script>
   export default {
-    name: "Input"
+    name: "Input",
+    data() {
+      return {
+        input: '',
+        inputPw: ''
+      }
+    }
   }
 </script>
 
